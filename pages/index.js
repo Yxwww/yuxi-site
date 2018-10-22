4924a9d5ea3f47c0461aa504bbbbb03e2dc9e22b
Yxwww <yuxi.wangcd@gmail.com>
2018-10-22 17:31:07 -0600

    Add deployment options

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
-import css from '../styles.scss';
+import Head from 'next/head'
 
-export default () => <div className={ css.example }> Welcome to next.js! </div>
+export default () => (
+  <div>
+    <Head>
+      <title>My page title</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+    </Head>
+    <Head>
+      <meta name="viewport" content="initial-scale=1.2, width=device-width" key="viewport" />
+    </Head>
+    <p>Hello world!</p>
+  </div>
+)
+
