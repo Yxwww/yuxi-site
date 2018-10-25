a1886947904b612eac83ab89cc1ab1de4a1adee9
Yxwww <yuxi.wangcd@gmail.com>
2018-10-24 22:04:36 -0600

    Add eslint, prettier
    - fix getInitialProp experience data import

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 import '../sass/index.scss';
+import { getExperience } from './contents';
 
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {}
-
-    // if (Component.getInitialProps) {
-    //   pageProps = await Component.getInitialProps(ctx)
-    // }
-
+  static async getInitialProps() {
+    const pageProps = { experience: getExperience() };
     return { pageProps }
   }
 
-  render () {
+  render() {
     const { Component, pageProps } = this.props
     return (
       <Container>
