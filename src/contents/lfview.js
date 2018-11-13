80c15591e94869071918e30ad2415be6c388bad8
Yxwww <yuxi.wangcd@gmail.com>
2018-11-13 09:22:19 -0700

    Move "lead developer" to description

--- src/contents/lfview.js
+++ src/contents/lfview.js
@@ -2,7 +2,6 @@ import { generate } from 'shortid'
 import { createExperience } from './constructors'
 
 const contributions = [
-  // 'lead developer of frontend stack',
   'Provide expertise and incorporate best practices of frontend stack.',
   'Design, propose, and implement advanced state control system with Redux and Rxjs',
   'Participate in design and implementation of the full aspect of frontend stack includes 2D/3D visualization, UI components, and overall application state control.',
@@ -15,7 +14,7 @@ const thumbnails = [
   'lfview-look-at-wing.png',
   'seequent-eagle-slides.png',
 ]
-const description = `Design and implement LeapFrog frontend stack from scratch, with test coverage of over 90%, to the Redux/Rxjs/Polymer/ThreeJS based 3D data visualization web application.\n
+const description = `Lead developer of frontend stack. Design and implement LeapFrog frontend stack from ground up, with test coverage of over 90%, to the Redux/Rxjs/Polymer/ThreeJS based 3D data visualization web application.\n
     Application went from initial research, design and development to production in less than 12 months and has continued with weekly deployment.`
 
 const time = 'June 2016 - Now'
