cff2ce86c0e39fe24dc4d939f45af60176f3695f
Yxwww <yuxi.wangcd@gmail.com>
2018-11-21 17:33:45 -0700

    Add ERWear contributions

--- src/contents/erwear.js
+++ src/contents/erwear.js
@@ -1,12 +1,16 @@
 import { generate } from 'shortid'
 import { createExperience } from './constructors'
 
-const contributions = []
+const contributions = [
+  'Conduct interview with first responders following IDEO methods. Analyze and incorporate design based on user feedback',
+  'Design and develop functional protpotype utilizes glass and writst based wearable hardware, and websocket based real time communication technology',
+  'Conduct research on usability study of a wearable computing solution for first responders',
+]
 
 const thumbnails = ['erwear-poster.png']
 
 const descrption =
-  'A wearable system to facilitate status monitoring, location tracking, real-time communications, and media transfer for emergency response.'
+  'Research, design and developed wearable system, utilizes on glass and wrist based wearable technology ,to enhance emergency responders situational awarenes.'
 const roles = ['Researcher', 'Software Developer']
 const time = 'September, 2015 - June, 2016'
 
--- src/contents/sod.js
+++ src/contents/sod.js
@@ -8,6 +8,8 @@ const thumbnails = ['sod-cropped.png']
 const descrption =
   'Designed and developed project SoD-Toolkit : A Toolkit for Interactively Prototyping and Developing Multi-Sensor, Multi-Device Environments. SoD uses Nodejs as server, SocketIO for communication, and adopts MS Kinect, Google Tango for providing spatial awareness.'
 
+const time = 'June, 2014 - June, 2016'
+const roles = ['Software Developer Intern', 'Research Assistent']
 let experience
 
 export function createSodExperience() {
@@ -19,6 +21,9 @@ export function createSodExperience() {
       thumbnails,
       descrption,
       contributions,
+      time,
+      '',
+      roles,
     )
   }
   return experience
