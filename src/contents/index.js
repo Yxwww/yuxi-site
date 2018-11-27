f2da8300f053dcfffb3eb2d7a60a24f4967c4513
Yxwww <yuxi.wangcd@gmail.com>
2018-11-26 21:03:44 -0700

    Add Uni bro content

--- src/contents/index.js
+++ src/contents/index.js
@@ -2,12 +2,13 @@ import { createLfviewExperience } from './lfview'
 import { createERWearExperience } from './erwear'
 // import { createEocfExperience } from './eocf'
 import { createSodExperience } from './sod'
+import { createMediaBroExperience } from './media-bro'
 
 export function getExperience() {
   return [
     createLfviewExperience(),
     createERWearExperience(),
     createSodExperience(),
-    // createEocfExperience(),
+      createMediaBroExperience(),
   ]
 }
---
+++ src/contents/media-bro.js
@@ -0,0 +1,33 @@
+import { generate } from 'shortid'
+import { createExperience } from './constructors'
+
+const contributions = [
+  'Developed and maintained media website for local community in Calgary using CMS.',
+  'Designed and developed web application to ease up vehicle service reservation process using Google Map API and jQuery-Mobile.',
+  'Polished website front end using jQuery and HTML/CSS. We managed to increase the visitor session number from 20 to 150 per day. Each session contains at least 5 visited pages.',
+]
+
+const thumbnails = []
+
+const descrption =
+  'Designed, developed and maintained community website. Coordinated design and development process. Researched and implemented software solution for various feature requests.'
+const roles = ['Technical Director, Developer']
+const time = 'October, 2013 - May, 2015'
+
+let experience
+export function createMediaBroExperience() {
+  if (!experience) {
+    experience = createExperience(
+      generate(),
+      'Uni Brothers Media Ltd',
+      'Community Website',
+      thumbnails,
+      descrption,
+      contributions,
+      time,
+      '',
+      roles,
+    )
+  }
+  return experience
+}
