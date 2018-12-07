7c2186c53310620b9f7f30985ffc53e700c1da20
Yxwww <yuxi.wangcd@gmail.com>
2018-12-06 18:13:10 -0700

    Add react-markdown

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 import Page from '../layouts/main'
 
-export default () => (
-  <Page>
-    <div>
-      <div className="container">
-        <h1>Home</h1>
-        <p>Hello traveller, welcome to my site !</p>
-        <p>
-          All written content on this site is provided under a Creative Commons ShareAlike license. All code is provided under a MIT license unless otherwise stated.
-        </p>
+export default props => {
+  const markdown = '## HELLO there'
+  return (
+    <Page>
+      <div>
+        <div className="container">
+          <h1>Home</h1>
+          <p>Hello traveller, welcome to my site !</p>
+          <ReactMarkdown source={markdown} />
+          <p>
+            All written content on this site is provided under a Creative
+            Commons ShareAlike license. All code is provided under a MIT license
+            unless otherwise stated.
+          </p>
+        </div>
       </div>
-    </div>
-  </Page>
-)
+    </Page>
+  )
+}
