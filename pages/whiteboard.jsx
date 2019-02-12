0ff0bf6c647e5b267178bc9e5740637198e9edba
Yxwww <yuxi.wangcd@gmail.com>
2019-02-12 16:37:21 -0700

    Fix restart dragging issue

--- pages/whiteboard.jsx
+++ pages/whiteboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { fromEvent } from 'rxjs'
-import { switchMap, tap, takeUntil } from 'rxjs/operators';
+import { switchMap, tap, takeUntil } from 'rxjs/operators'
 import '../sass/whiteboard.scss'
 
 const useMouseDrag = containerRef => {
@@ -15,22 +15,23 @@ const useMouseDrag = containerRef => {
     const mv$ = fromEvent(canvas, 'mousemove').pipe(
       tap(e => {
         setPosition([e.clientX, e.clientY])
-      })
+      }),
     )
     const md$ = fromEvent(canvas, 'mousedown')
     const drawing$ = md$.pipe(
       tap(() => setInteraction('down')),
-      switchMap(() => mv$),
-      tap(() => setInteraction('dragging')),
-      tap(e => {
-        console.log(e)
-        return e
-      }),
-      takeUntil(mu$),
+      switchMap(() =>
+        mv$.pipe(
+          tap(() => setInteraction('dragging')),
+          takeUntil(mu$),
+        ),
+      ),
     )
 
-    const sub = drawing$.subscribe(data => {
-      console.log(data)
+    const sub = drawing$.subscribe({
+      complete: data => {
+        console.log('complete', data)
+      },
     })
     return () => {
       sub.unsubscribe()
