f002c29a51454d3a5c4182de1b31b57a20d87f28
Yxwww <yuxi.wangcd@gmail.com>
2019-06-14 22:18:13 -0600

    Remove home page blurb

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,13 +4,13 @@ import Page from '../layouts/main'
 export default () => (
   <Page>
     <div>
-        <h1>Home</h1>
-        <p>Hello traveller, welcome to my site !</p>
-        <p>
-          All written content on this site is provided under a Creative Commons
-          ShareAlike license. All code is provided under a MIT license unless
-          otherwise stated.
-        </p>
+      <h1>Home</h1>
+      <p>Hello traveller, welcome to my site !</p>
+      {/* <p> */}
+      {/*   All written content on this site is provided under a Creative Commons */}
+      {/*   ShareAlike license. All code is provided under a MIT license unless */}
+      {/*   otherwise stated. */}
+      {/* </p> */}
     </div>
   </Page>
 )
