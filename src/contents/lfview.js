b685d1ff347bee007c9796236fa59af8cbca60a8
Yuxi Wang <yuxi.wangcd@gmail.com>
2019-10-22 10:13:06 -0600

    Simplify resume

--- src/contents/lfview.js
+++ src/contents/lfview.js
@@ -9,19 +9,15 @@ const thumbnails = [
 ]
 
 const contributions = [
-  'Researched and introduced emerging technologies and methodologies such as Flux(redux), rxjs, TypeScript, css grid system, and Functional Programming paradigm.',
-  'Participated in design and implementation of all aspects of frontend stack including: UI programming, graphic programming, business logic implementation, and application data flow control.',
-  'Maintained the project through critical development life cycle including ideation, research, proposal, development, and maintenance.',
-  'Maximize productivity by balancing between various testing philosophies while maintaining over 95% test coverage.',
-  'Designed architecture to handle large datasets within business logic and visualization components.',
-  'Diagnosed and improved application performance from 10 fps to 50 fps by improving rendering process.',
+  'Researched and introduced emerging technologies, design patterns and programming paradigms.',
+  'Participated in design and implementation of all aspects of frontend including: visualization library, UI programming, and application dataflow control.',
+  'Maintained projects through critical life cycle including ideation, research, proposal, development, maintenance and sunset.',
+  'Researched and improved application rendering and memory performance.',
   'Onboarded new team members by providing training materials, documentation, and mentorship.',
-  'Researched and standardized industry coding style. Introduced and maintained linter, formatter into development workflow.',
 ]
 
 const description = `
-Frontend developer of 3D data visualization web application LFview. Led frontend stack redesign from ground up to the React/Redux/Rxjs/Polymer/ThreeJS based web app.
-Project went from initial research, design and development, to delivery of an enterprise grade product in less than 12 months and has continued with continuous deployment.
+Frontend developer of 3D data visualization web application LFView. Reserached and developed frontend UI and visualization redesign from ground up with technologies such as Redux/React(Hooks API), Rxjs, Svelte, and TypeScript etc. Project went from initial research, design and development, to delivery of an enterprise grade product in less than 12 months and has continued with weekly continuous deployment.
 `
 
 const roles = [DEVELOPER_ROLE]
