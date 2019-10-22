9d145aafc642c85eec51aaaa42091e76d5cc8091
Yuxi Wang <yuxi.wangcd@gmail.com>
2019-10-22 10:17:45 -0600

    Remove mediabro exp

--- src/contents/index.js
+++ src/contents/index.js
@@ -2,13 +2,13 @@ import { createLfviewExperience } from './lfview'
 import { createERWearExperience } from './erwear'
 // import { createEocfExperience } from './eocf'
 import { createSodExperience } from './sod'
-import { createMediaBroExperience } from './media-bro'
+// import { createMediaBroExperience } from './media-bro'
 
 export function getExperience() {
   return [
     createLfviewExperience(),
     createERWearExperience(),
     createSodExperience(),
-    createMediaBroExperience(),
+    // createMediaBroExperience(),
   ]
 }
