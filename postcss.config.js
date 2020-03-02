509fc7151ad7fa7ce526a0f80b2a213a9e0ceb8e
Yuxi Wang <yuxi.wangcd@gmail.com>
2020-03-02 16:46:18 -0700

    Fix build purgecss config

--- postcss.config.js
+++ postcss.config.js
@@ -5,15 +5,18 @@ module.exports = {
     'autoprefixer',
     ...(process.env.NODE_ENV === 'production'
       ? [
-          '@fullhuman/postcss-purgecss',
-          {
-            content: [
-              './components/**/*.js',
-              './pages/**/*.js',
-              './layouts/**/*.js',
-            ],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
+          [
+            '@fullhuman/postcss-purgecss',
+            {
+              content: [
+                './components/**/*.js',
+                './pages/**/*.js',
+                './layouts/**/*.js',
+              ],
+              defaultExtractor: content =>
+                content.match(/[\w-/:]+(?<!:)/g) || [],
+            },
+          ],
         ]
       : []),
   ],
