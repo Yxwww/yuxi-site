fc7af1248554c37feb5afb7161f1fb92e37a41b7
Yuxi Wang <yuxi.wangcd@gmail.com>
2020-03-02 15:36:17 -0700

    Fix postcss deps

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,23 @@
 // const tailwindcss = require('tailwindcss')
 // const postcss = require('postcss-preset-env')
+/* eslint-disable */
 
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
     ...(process.env.NODE_ENV === 'production'
-      ? {
-          '@fullhuman/postcss-purgecss': {
+      ? [
+          require('@fullhuman/postcss-purgecss')({
             content: [
               './components/**/*.js',
               './pages/**/*.js',
               './layouts/**/*.js',
             ],
             defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        }
-      : {}),
-  },
+          }),
+        ]
+      : []),
+  ],
 }
