884923b3003ded8f9c8434baa59804954344db81
Yuxi Wang <yuxi.wangcd@gmail.com>
2020-06-29 16:54:44 -0600

    Update to nextjs/canary with mdx support

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-// const { pipe } = require('ramda')
-// const withMdx = require('@zeit/next-mdx')({
-//   extension: /.mdx?$/,
-// })
+const withMDX = require('@next/mdx')()
 
-// const plugins = pipe(withMdx)
-
-// next.config.js
-module.exports = () => ({
-  // ...plugins({
-  //   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  //   cssModules: false,
-  // }),
-  target: 'serverless',
-  crossOrigin: 'anonymous',
-})
+module.exports = withMDX()
