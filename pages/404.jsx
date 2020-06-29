2d9d627980705cd65ced03669583bf2c8f151588
Yuxi Wang <yuxi.wangcd@gmail.com>
2020-06-29 17:38:56 -0600

    Add 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
   return (
     <Page>
       <div className="text-xl mx-auto vertical-align text-center left-0 right-0">
-        Something went wrong :( <br />
+        404. Sorry we couldn&apos;t find what you were looking for. :( <br />
       </div>
     </Page>
   )
