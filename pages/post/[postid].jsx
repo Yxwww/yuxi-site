301b3177ff8793c850df06219bbd50c816c74d50
Yxwww <yuxi.wangcd@gmail.com>
2020-06-28 19:45:46 -0600

    Update Page default to named export

--- pages/post/[postid].jsx
+++ pages/post/[postid].jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
-import Page from '../../components/layouts/main'
+import { Page } from '../../components/layouts/main'
 
 const Markdown = dynamic(() => import('../../components/code/Markdown.jsx'))
 
--- pages/post/erwear.jsx
+++ pages/post/erwear.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import dynamic from 'next/dynamic'
-import Page from '../../components/layouts/main'
+import { Page } from '../../components/layouts/main'
 import { ERWearExperience } from '../../src/contents'
 
 const ExperienceSection = dynamic(() =>
