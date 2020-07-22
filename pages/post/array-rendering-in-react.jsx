73f9865bcd7426a62e6a5ffacfec7e43432dc22f
Yuxi Wang <yuxi.wangcd@gmail.com>
2020-07-22 14:43:32 -0600

    Add React.memo component

--- pages/post/array-rendering-in-react.jsx
+++ pages/post/array-rendering-in-react.jsx
@@ -2,15 +2,40 @@ import React, { useState, memo } from 'react'
 
 const content1 = { id: '1', content: 'content 1' }
 const content2 = { id: '2', content: 'content 2' }
-const ContentDisplayMemo = memo(function ContentDisplay({ content }) {
+function ContentDisplay({ content, tag }) {
+  console.log('rendered content for id: ', content.id, ` as ${tag}`)
   return <pre>{JSON.stringify(content)}</pre>
-})
+}
+
+const ContentDisplayMemo = memo(ContentDisplay)
 
 function ReRenderList({ contents }) {
   return (
     <div>
       {contents.map(content => {
-        return <ContentDisplayMemo key={content.id} content={content} />
+        return (
+          <ContentDisplay
+            key={content.id}
+            content={content}
+            tag="Non memo component"
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+function ReactMemoDisplay({ contents }) {
+  return (
+    <div>
+      {contents.map(content => {
+        return (
+          <ContentDisplayMemo
+            key={content.id}
+            content={content}
+            tag="Memoized component"
+          />
+        )
       })}
     </div>
   )
@@ -18,11 +43,16 @@ function ReRenderList({ contents }) {
 
 export default function TestPage() {
   const [contents, updateContents] = useState([content1, content2])
+  console.log('contents updated', contents)
   return (
     <div className="absolute h-full grid">
       <div className="self-center">
+        <p>List always renders</p>
         <ReRenderList contents={contents} />
-        <div>
+        <hr />
+        <p>List only renders when content changed</p>
+        <ReactMemoDisplay contents={contents} />
+        <div className="mt-4">
           <button
             type="button"
             onClick={() => {
