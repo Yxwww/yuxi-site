e41709e1d729aa5e73e1a16a218892bf0d8222b7
Yuxi Wang <yuxi.wangcd@gmail.com>
2022-07-15 12:28:38 -0600

    Add markdoc next

--- pages/markdoc.jsx
+++ pages/markdoc.jsx
@@ -1,11 +1,12 @@
-import Markdoc from '@markdoc/markdoc';
-import React from 'react';
+import Markdoc from '@markdoc/markdoc'
+import React from 'react'
 
 export default function MarkdocPage() {
   const source = `
   ## Hello from markdoc
-  `;
-  const ast = Markdoc.parse(source);
-  const content = Markdoc.transform(ast, /* config */);
+  `
+  const ast = Markdoc.parse(source)
+  console.log('ast', ast)
+  const content = Markdoc.transform(ast /* config */)
   return Markdoc.renderers.react(content, React)
 }
