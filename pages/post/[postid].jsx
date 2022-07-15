443b67ad3ad202b1d128b63ac01afe5c6439cafe
Yuxi Wang <yuxi.wangcd@gmail.com>
2022-07-15 12:36:19 -0600

    Remove markdown reference in post

--- pages/post/[postid].jsx
+++ pages/post/[postid].jsx
@@ -4,24 +4,13 @@ import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
 import { Page } from '../../components/layouts/main'
 
-const Markdown = dynamic(() => import('../../components/code/Markdown.jsx'))
-
-function Content({ markdown, title }) {
-  return (
-    <div>
-      <h1 style={{ textTransform: 'capitalize' }}>{title}</h1>
-      <Markdown value={markdown} />
-    </div>
-  )
-}
-
 const Post = ({ markdown }) => {
   const {
     query: { postid: title },
   } = useRouter()
   return (
     <Page>
-      <Content markdown={markdown} title={title} />
+      <div>POST</div>
     </Page>
   )
 }
