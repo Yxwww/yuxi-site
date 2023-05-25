2056202aaa9109c8f3f73c2ff1a15d4d624f252c
Yuxi Wang <yuxi.wangcd@gmail.com>
2023-05-25 15:49:47 -0700

    Fix profile image url

--- src/contents/constants.ts
+++ src/contents/constants.ts
@@ -6,4 +6,4 @@ export const ROLE_RESEARCH_ASSISTANT = 'Research Assistant'
 
 export const EXTERNAL_URL = 'https://yuxi.dev'
 export const EXTERNAL_POSTS_URL = `${EXTERNAL_URL}/posts`
-export const PROFILE_IMAGE_URL = '/static/img/kinect-infra-red.jpeg'
+export const PROFILE_IMAGE_URL = `${EXTERNAL_URL}/static/img/kinect-infra-red.jpeg`
