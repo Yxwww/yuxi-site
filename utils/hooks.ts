55c98682dbd70e7ec8acf690893b59e026b23aa9
Yuxi Wang <yuxi.wangcd@gmail.com>
2023-05-31 22:21:40 -0700

    Remove console.log

--- utils/hooks.ts
+++ utils/hooks.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react';
 
 // source: https://usehooks.com/useLocalStorage/
 export function useLocalStorage<T>(
@@ -9,57 +9,57 @@ export function useLocalStorage<T>(
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
     if (typeof window === 'undefined') {
-      return initialValue
+      return initialValue;
     }
     try {
       // Get from local storage by key
-      const item = window.localStorage.getItem(key)
+      const item = window.localStorage.getItem(key);
       // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue
+      return item ? JSON.parse(item) : initialValue;
     } catch (error) {
       // If error also return initialValue
-      console.log(error)
-      return initialValue
+      console.error(error);
+      return initialValue;
     }
-  })
+  });
 
   // setValue should have no side effects
   const setValue = useCallback((value: T | ((value: T) => void)) => {
-    setStoredValue(value)
-  }, [])
+    setStoredValue(value);
+  }, []);
 
   useEffect(() => {
     try {
-      const stringified = JSON.stringify(storedValue)
+      const stringified = JSON.stringify(storedValue);
       if (
         typeof window !== 'undefined' &&
         window.localStorage.getItem(key) !== stringified
       ) {
-        window.localStorage.setItem(key, stringified)
+        window.localStorage.setItem(key, stringified);
       }
     } catch (error) {
       // A more advanced implementation would handle the error case
-      console.log(error)
+      console.error(error);
     }
-  }, [key, storedValue])
+  }, [key, storedValue]);
 
-  return [storedValue, setValue]
+  return [storedValue, setValue];
 }
 
 // source: https://designcode.io/react-hooks-handbook-usescrollposition-hook
 function useScrollPosition() {
-  const [scrollPosition, setScrollPosition] = useState(0)
+  const [scrollPosition, setScrollPosition] = useState(0);
 
   useEffect(() => {
     const updatePosition = () => {
-      setScrollPosition(window.pageYOffset)
-    }
-    window.addEventListener('scroll', updatePosition)
-    updatePosition()
-    return () => window.removeEventListener('scroll', updatePosition)
-  }, [])
+      setScrollPosition(window.pageYOffset);
+    };
+    window.addEventListener('scroll', updatePosition);
+    updatePosition();
+    return () => window.removeEventListener('scroll', updatePosition);
+  }, []);
 
-  return scrollPosition
+  return scrollPosition;
 }
 
-export default useScrollPosition
+export default useScrollPosition;
