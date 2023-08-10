0004783f97e7cbee0bbb5cba33ad6126d7280de4
Yuxi Wang <yuxi.wangcd@gmail.com>
2023-08-10 11:21:04 -0700

    Improve readingtime alignment

--- components/ReadingTime.tsx
+++ components/ReadingTime.tsx
@@ -2,7 +2,7 @@ import CoffeeIcon from './icons/Coffee';
 
 export default function ReadingTime({ readingTime }: { readingTime: number }) {
   return (
-    <span className="inline-flex items-center" title="reading time">
+    <span className="inline-flex items-end" title="reading time">
       <CoffeeIcon className="inline-block w-5 h-5 mr-2" />{' '}
       <span className="leading-tight mr-6">{readingTime} mins </span>
     </span>
