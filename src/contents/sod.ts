06662ca9ad7b807f3851fd8ff295f01db4e73d6e
Yuxi Wang <yuxi.wangcd@gmail.com>
2023-08-29 16:47:16 -0700

    Update content

--- src/contents/sod.ts
+++ src/contents/sod.ts
@@ -5,6 +5,8 @@ import { createERWearExperience } from './erwear';
 
 const contributions = [
   'Developed and maintained the SoD-Toolkit, a web-based toolkit for prototyping multi-sensor, multi-device environments.',
+  'Designed and implemented 2D and 3D visualizer. Utilized html5 canvas and Unity to provide visual demonstration of server state in realtime.',
+  'Developed and maintained client library in various platforms including C#, Obj-C, JavaScript, Android, and Unity.',
   'Designed server architecture for real-time state control and geometric computation.',
   'Integrated the toolkit with various hardware technologies, including MS Kinect, Google Tango, and Leap Motion.',
 ];
